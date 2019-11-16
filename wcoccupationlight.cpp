0557d4e3f5eafe6cd2771999d77b5c8a0e4e2e16
Leonhard Künzler <leonhard@kuenzler.io>
2019-11-16 15:51:56 +0100

    Initial commit

---
+++ wcoccupationlight.cpp
@@ -0,0 +1,216 @@
+#include "wcoccupationlight.h"
+
+#include <ESP8266WiFi.h>
+#include <ArduinoJson.h>
+#include <ESP8266httpUpdate.h>
+#include <ESP8266WebServer.h>
+#include <ESP8266HTTPUpdateServer.h>
+
+const char* ssid = "your-ssid";
+const char* password = "your-password";
+
+const int sensorSwitch = D3;
+
+Light red    { "red", D5, false, false };
+Light green  { "green", D6, false, false };
+
+bool isOccupied = false;
+bool isActive = true;
+
+long lastSwitch = 0;
+long lastReservation = 0;
+long lastBlink = 0;
+long lastCheck = 0;
+
+const long reservationTimeout = 10*60*1000; // 10 min
+const long activeTimeout = 2*60*60*1000; // 2 std
+const long checkIntervall = 100; // 100 ms
+
+const int readingCount = 5;
+bool readings[readingCount];
+int currentReading = 0;
+const double readingOffset = 0.5;
+
+const int sensorPin = D1;
+
+ESP8266WebServer webServer(80);
+ESP8266HTTPUpdateServer httpUpdater;
+WiFiClient client;
+
+//The setup function is called once at startup of the sketch
+void setup() {
+
+	pinMode(sensorPin, INPUT);
+
+	pinMode(red.switchPin, OUTPUT);
+	pinMode(green.switchPin, OUTPUT);
+
+	Serial.begin(9600);
+
+	pinMode(LED_BUILTIN, OUTPUT);
+	digitalWrite(sensorSwitch, LOW);
+
+	setLightOn(red, true);
+	setLightOn(green, true);
+
+	WiFi.mode(WIFI_STA);
+	Serial.println();
+	Serial.print("Connecting to ");
+	Serial.println(ssid);
+
+	WiFi.begin(ssid, password);
+	WiFi.hostname("toiletTrafficLight");
+
+//	while (WiFi.status() != WL_CONNECTED) {
+//		delay(500);
+//		Serial.print(".");
+//	}
+//	Serial.println("");
+//	Serial.println("WiFi connected");
+
+	// web server stuff
+	webServer.on("/", handleRoot);
+	webServer.onNotFound(handleRoot);
+	webServer.on("/inline", []() {
+		webServer.send(200, "text/plain", "this works as well");
+	});
+
+	httpUpdater.setup(&webServer);
+
+	webServer.begin();
+	Serial.println("Server started");
+	Serial.println(WiFi.localIP());
+
+	// pre-fill array
+	for(int i = 0; i < readingCount; i++){
+		readSensor();
+	}
+
+	// set initial state
+	if (getOccupied()) {
+		// sensor reports occupied, but light was not changed yet
+		isOccupied = true;
+		lastSwitch = millis();
+		setLightOn(red, true);
+		setLightOn(green, false);
+	} else {
+		// sensor reports not occupied, but light was not changed yet
+		isOccupied = false;
+		lastSwitch = millis();
+		setLightOn(red, false);
+		setLightOn(green, true);
+	}
+}
+// The loop function is called in an endless loop
+void loop() {
+
+	if (millis()-lastCheck > checkIntervall) {
+		// check active timeout -> save energy
+		if (isActive && !isOccupied && (millis() - lastSwitch > activeTimeout)) {
+			setLightOn(green, false);
+			isActive = false;
+		}
+
+		// check occupation, only act on changes
+		readSensor();
+		if(getOccupied() && !isOccupied){
+			// sensor reports occupied, but light was not changed yet
+			isOccupied = true;
+			lastSwitch = millis();
+			setLightOn(red, true);
+			setLightOn(green, false);
+			isActive = true;
+		} else if(!getOccupied() && isOccupied){
+			// sensor reports not occupied, but light was not changed yet
+			isOccupied = false;
+			lastSwitch = millis();
+			setLightOn(red, false);
+			setLightOn(green, true);
+		}
+
+		handleBlink();
+		handleReservation();
+		lastCheck = millis();
+	}
+
+	webServer.handleClient();
+}
+
+void readSensor(){
+	// currentReading is not changed elsewhere, no check for oob
+	readings[currentReading++] = readSensorRaw();
+	if (currentReading > readingCount) {
+		currentReading = 0;
+	}
+}
+
+int readSensorRaw(){
+	return digitalRead(sensorPin);
+}
+
+void setLightOn(Light& light, bool on){
+	digitalWrite(light.switchPin, on ? LOW : HIGH);
+	light.isOn = on;
+}
+
+void setLightBlinking(Light& light, bool on) {
+	light.isBlinking = on;
+}
+
+void setAllBlinking(bool on){
+	setLightBlinking(red, on);
+	setLightBlinking(green, on);
+}
+
+bool getOccupied(){
+	double result = 0;
+	for(int i = 0; i < readingCount; i++){
+		result += readings[i];
+	}
+	result = result / readingCount;
+	return (result < readingOffset);
+}
+
+void handleBlink() {
+
+}
+
+void handleReservation() {
+
+}
+
+void handleRoot() {
+	String response;
+
+	response += "Toilet occupation<br><br>";
+	String redOn = (red.isOn ? "on" : "off");
+	response += "red light: "+ redOn + "<br>";
+	String greenOn = (green.isOn ? "on" : "off");
+	response += "green light: "+ greenOn + "<br>";
+	response += "<br>";
+	String occupied = (isOccupied ? "yes" : "no");
+	response += "occupied: "+ occupied + "<br>";
+	response += "<br>";
+	if((red.isOn != green.isOn) && isActive){
+		if(isOccupied){
+			response += "occupied for ";
+		} else {
+			response += "free for ";
+		}
+		response += String((millis() - lastSwitch)/60/1000) + " minutes";
+	}
+
+	if (!isActive) {
+		response += "inactive for "+ String((millis() - activeTimeout)/60/1000) + " minutes";
+	}
+	response += "<br><br>";
+
+	response += "uptime: " + String(millis() / 60 / 60 / 1000) + " hours";
+	if (isActive) {
+		response += "<br>";
+		response += "will go inactive in " + String(-(millis() - lastSwitch - activeTimeout) / 60 / 1000) + " minutes";
+	}
+
+
+	webServer.send(200, "text/html", response);
+}
---
+++ wcoccupationlight.h
@@ -0,0 +1,32 @@
+#ifndef _wcoccupationlight_H_
+#define _wcoccupationlight_H_
+
+#include "Arduino.h"
+
+struct Light {
+	String name;
+	int switchPin;
+	bool isBlinking;
+	bool isOn;
+};
+
+void setLightOn(Light& light, bool on);
+void setLightBlinking(Light& light, bool on);
+void setAllBlinking(bool on);
+
+bool getLightOn(Light& light);
+bool getLightBliking(Light& light);
+
+void reserve();
+bool getOccupied();
+
+int readSensorRaw();
+void readSensor();
+
+void handleReservation();
+void handleBlink();
+
+void handleRoot();
+void handleSetLight();
+
+#endif /* _wcoccupationlight_H_ */
